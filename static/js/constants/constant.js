128dac549c305670dcfcd795f3dfb643c5988829
brajesh anokha <brajesh.anokha@sapient.com>
2019-02-05 12:37:10 +0530

    1

--- static/js/constants/constant.js
+++
@@ -1,4 +0,0 @@
-// export const BACKEND_URL = 'http://localhost:3000/';
-// export const BACKEND_URL = 'https://scalable-auth-with-docker.com/';
-
-export const BACKEND_URL = 'http://ec2-13-232-10-111.ap-south-1.compute.amazonaws.com:3000/';
